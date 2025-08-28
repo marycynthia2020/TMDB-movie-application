9fccd0fc4cd7ed5fc47539ba3000506cb82ca028
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-08-28 01:04:38 +0000

    add top rated and upcoming movies

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,39 +1,39 @@
 "use client";
 import DetailsCard from "@/component/DetailsCard";
+import Spinner from "@/component/Spinner";
 import { useGetMovieDetails } from "@/Hooks/use-getMovieDetails";
+import { movieDetails } from "@/types";
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 const MovieDetailsPage = () => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState<movieDetails>();
   const { id } = useParams();
   const { data, isLoading, isError } = useGetMovieDetails(`${id}`);
 
   useEffect(() => {
     if (data) {
-        console.log(data)
       setMovie(data);
     }
   }, [data]);
 
-  if (isLoading) {
+  if (isLoading || isError) {
+    return <Spinner />;
+  }
+
+  if (!movie) {
     return (
-      <div className="w-full max-w-[1260px] mx-auto px-4 text-center">
-        loading...
+      <div className="w-full max-w-[1260px] mx-auto px-4 text-2xl">
+        Movie not found
       </div>
     );
   }
 
-  if (isError) {
-    return (
-      <div className="w-full max-w-[1260px] mx-auto px-4 text-center">
-        There was an error loading movies
-      </div>
-    );
-  }
-  return <div className="w-full max-w-[1260px] mx-auto px-4 " >
-    <DetailsCard movie={movie} />
-  </div>;
+  return (
+    <div className="w-full max-w-[1260px] mx-auto px-4 ">
+      <DetailsCard movie={movie} />
+    </div>
+  );
 };
 
 export default MovieDetailsPage;
